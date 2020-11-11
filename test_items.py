c41d3ab78e147ab4fe32e31e9e459a8191fa04ff
Олег <44393834+astapenko2018@users.noreply.github.com>
2020-11-11 22:13:31 +0300

    Добавление файлов для проверки

---
+++ test_items.py
@@ -0,0 +1,17 @@
+import time
+import pytest
+from selenium.common.exceptions import NoSuchElementException
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def test_page_contains_button_add_to_basket(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("[id=language_selector]>.btn").click()
+
+#Удалить коментарий для запуска с --language=fr
+    #time.sleep(30)
+
+    try:
+        browser.find_element_by_css_selector(".btn-add-to-basket")
+    except NoSuchElementException:
+        pytest.fail("There is no such element")
+
